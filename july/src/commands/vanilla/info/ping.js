de78ab7f625d82711017b490faa409942e084dc0
eloilsonfontenele2@gmail.com <eloilsonfontenele2@gmail.com>
2022-05-03 21:45:49 -0300

    Primeiro commit

--- july/src/commands/vanilla/info/ping.js
+++ july/src/commands/vanilla/info/ping.js
@@ -5,20 +5,25 @@ const Vanilla = Command.Vanilla
 
 module.exports = class extends Vanilla {
   constructor(ctx) {
-    super(ctx,{
+    super(ctx, {
       name: 'ping',
       aliases: ['pin'],
       avaliable: true
     })
 
   }
-  async run(){
-		var desc = this.ctx.locale.commands.ping.text.replace('{ping}',this.ctx.client.ws.ping)
-		console.log(desc)
-		let embed = new Embed(this.ctx.message.author)
-		.setDescription(`${desc}`)
-		this.ctx.message.reply({
-			embeds: [embed]
-		})
+  async run() {
+    var desc = this.ctx.locale.command.ping.text
+    .replace(
+      '{ping}',
+      this.ctx.client.ws.ping
+    )
+
+    var embed = new Embed(this.ctx.message.author)
+      .setDescription(`${desc}`)
+
+    this.ctx.message.reply({
+      embeds: [embed]
+    })
   }
-	}
+}
